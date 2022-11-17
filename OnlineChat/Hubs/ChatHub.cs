a576855da16161d427446f2072b9331e66f34465
artemgalyan <89996510+artemgalyan@users.noreply.github.com>
2022-11-17 21:09:52 +0300

    init

---
+++ OnlineChat/Hubs/ChatHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace OnlineChat.Hubs;
+
+public class ChatHub : Hub
+{
+    public async Task Send(string name, string message)
+    {
+        await Clients.All.SendCoreAsync("Send", new object?[]{ name, message });
+    }
+}
---
+++ OnlineChat/wwwroot/index.js
@@ -0,0 +1,33 @@
+﻿const hubConnection = new signalR.HubConnectionBuilder()
+                    .withUrl("/chat")
+                    .build();
+
+document.getElementById('send-message-button').disabled = true;
+
+hubConnection.start().then(function () {
+    document.getElementById('send-message-button').disabled = false;
+    console.log("started...");
+}).catch(err => console.log("hey!\n"+err)); 
+
+function getNickname() {
+    return document.getElementById('nickname-input').value;
+}
+
+hubConnection.on("Send", function(name, message) {
+    const element = document.createElement("li")
+    element.textContent = name + ": " + message;
+    console.log(message);
+    const firstElement = document.getElementById("discussion").firstChild;
+    document.getElementById("discussion").insertBefore(element, firstElement);  
+});
+
+document.getElementById("send-message-button")
+        .addEventListener("click", () => {
+            const message = document.getElementById('message-input').value;
+            const nick = getNickname();
+            if (nick === undefined || nick === '') {
+                alert('You need to input nickname to send messages!');
+            } else {
+                hubConnection.invoke("Send", nick, message).then(result => console.log(result));
+            }
+        });
