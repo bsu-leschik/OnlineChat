ea9b9addbeada8b2d16236724094513329b5ddea
artemgalyan <89996510+artemgalyan@users.noreply.github.com>
2023-01-22 19:13:25 +0300

    Added passwordhasher tests

---
+++ Backend/UnitTests/Other/PasswordHasherTests.cs
@@ -0,0 +1,21 @@
+﻿using Database.Entities;
+using Microsoft.AspNetCore.Identity;
+using OnlineChat;
+
+namespace UnitTests.Other;
+
+public class PasswordHasherTests
+{
+    [Fact]
+    public void PasswordHasher_SuccessfullyVerifiesTruePassword()
+    {
+        const string username = "username";
+        const string password = "password";
+        var user = new User(username, password);
+        var hasher = new PasswordHasher();
+        var hashed = hasher.HashPassword(user, password);
+        user.Password = hashed;
+        var verificationResult = hasher.VerifyHashedPassword(user, hashedPassword: hashed, providedPassword: password);
+        Assert.Equal(verificationResult, PasswordVerificationResult.Success);
+    }
+}
