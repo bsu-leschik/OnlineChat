a36325f4a9090d3a48cf9be287498a89b12275d2
Дмитрий Л <fpm.leschik@bsu.by>
2023-02-06 11:40:35 +0300

    -

--- Frontend/src/app/shared/services/storage.service.ts
+++ Frontend/src/app/shared/services/storage.service.ts
@@ -6,7 +6,9 @@ import { Injectable } from '@angular/core';
 export class StorageService {
   private readonly data: Record<string, any> = {};
   public isLoggedIn = false;
-  constructor() { }
+
+  constructor() {}
+
   public set(key: string, value: any) : void {
     this.data[key] = value;
   }
